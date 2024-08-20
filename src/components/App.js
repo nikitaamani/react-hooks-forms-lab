7f63e6c3546a6fbb5df0d3ae501446bf44ffbf10
nikita <nikita.bosire@student.moringaschool.com>
2024-08-20 18:57:07 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
@@ -10,11 +11,14 @@ function App() {
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+  function handleItemFormSubmit(newItem){
+    setItems([...items,newItem])
+  }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
